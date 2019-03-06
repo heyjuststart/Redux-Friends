042c55233e2914d3968bff7fd435f805349a15ce
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-03-06 11:58:12 -0500

    fix nested project package.jsons
    Before this change, I was installing packages at the root level that
    should have been inside the CRA folder.
    
    This change undoes all of that and has things installing in the proper
    places.

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './reducers';
+import './index.css';
+import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
--- friends/src/reducers/friendsReducer.js
+++ friends/src/reducers/friendsReducer.js
@@ -1,5 +1,26 @@
+import {
+  FETCH_FRIENDS,
+  FETCH_FRIENDS_SUCCESS,
+  REQUEST_ERROR,
+  SAVE_FRIEND,
+  SAVE_FRIEND_SUCCESS,
+  UPDATE_FRIEND,
+  UPDATE_FRIEND_SUCCESS,
+  DELETE_FRIEND,
+  DELETE_FRIEND_SUCCESS
+} from '../actions';
+
 const initialState = {
-  friends: []
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [],
+  error: null
 };
 
 export const friendsReducer = (state = initialState, action) => {
